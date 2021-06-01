069f356e60f10c029a861ed8451f20a4ab4b3c31
ildarjan <nobap84@rambler.ru>
2021-06-01 20:46:51 +0300

    extract 'playGame' function as common for each game

--- src/common.js
+++ src/common.js
@@ -1,9 +1,40 @@
-export const printWinGame = (name) => console.log(`Congratulations, ${name}!`);
+import readlineSync from "readline-sync";
+import greetPlayer from "./games/brain-greeting-game.js";
+import {GAME_ROUND} from "./const.js";
 
-export const printLoseGame = (name) => console.log(`Let's try again, ${name}!`);
+const printWinGame = (name) => console.log(`Congratulations, ${name}!`);
+const printLoseGame = (name) => console.log(`Let's try again, ${name}!`);
+const printCorrect = () => console.log(`Correct!`);
+const printConditions = (condition) => console.log(condition);
+const printQuestion = (question) => console.log(`Question ${question}`);
+const printWrongAnswer = (correct, wrong) => (
+    console.log(`${wrong} is wrong answer ;(. Correct answer was ${correct}.`)
+)
 
-export const printCorrect = () => console.log(`Correct!`);
+const printGameOver = (name, correct, wrong) => {
+    printWrongAnswer(correct, wrong);
+    printLoseGame(name)
+}
 
-export const printConditions = (condition) => console.log(condition);
+const getAnswer = () => readlineSync.question(`Your answer `).trim().toLowerCase();
 
-export const printQuestion = (question) => console.log(`Question ${question}`);
+export const playGame = (conditions, questionGenerator) => () => {
+    const playerName = greetPlayer();
+
+    printConditions(conditions);
+
+    const playRound = (round, correctAnswer = false) => {
+        if (!round) { return printWinGame(playerName) }
+        if (correctAnswer) { printCorrect() }
+        const [num, correctAns] = questionGenerator();
+        printQuestion(num);
+        const userAns = getAnswer();
+        return (
+            userAns === correctAns ?
+                playRound(--round, true) :
+                printGameOver(playerName, correctAns, userAns)
+        );
+    }
+
+    return playRound(GAME_ROUND)
+}
