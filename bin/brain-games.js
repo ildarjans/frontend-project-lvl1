b099ae7e04228ca8d46cf81dca6cbf5b1076d911
ildarjan <nobap84@rambler.ru>
2021-06-08 16:42:16 +0300

    update greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 import brainGreetingGame from "../src/games/brain-greeting-game.js";
 
-console.log(`Welcome to the Brain Games!`);
-
 brainGreetingGame();
--- src/games/brain-greeting-game.js
+++ src/games/brain-greeting-game.js
@@ -2,9 +2,11 @@ import readlineSync from "readline-sync";
 
 const askPlayerName = () => readlineSync.question('May i have your name? ');
 
+const printWelcome = () => console.log(`Welcome to the Brain Games!`);
+
 const greeting = (name) => {
     console.log(`Hello, ${name}!`);
-    return name;
+    return name
 };
 
-export default () => greeting(askPlayerName());
+export default () => greeting(askPlayerName(printWelcome()));
