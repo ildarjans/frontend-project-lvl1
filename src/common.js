cc08edf27a3f35e1ba153a09c1092e47cb11574b
ildarjans <nobap84@rambler.ru>
2021-06-09 14:28:36 +0300

    use increment fn

--- src/common.js
+++ src/common.js
@@ -1,40 +1,41 @@
-import readlineSync from "readline-sync";
-import greetPlayer from "./games/brain-greeting-game.js";
-import {GAME_ROUND} from "./const.js";
+import readlineSync from 'readline-sync';
+import greetPlayer from './games/brain-greeting-game.js';
+import { GAME_ROUND } from './const.js';
+import { dec } from './utils.js';
 
 const printWinGame = (name) => console.log(`Congratulations, ${name}!`);
 const printLoseGame = (name) => console.log(`Let's try again, ${name}!`);
-const printCorrect = () => console.log(`Correct!`);
+const printCorrect = () => console.log('Correct!');
 const printConditions = (condition) => console.log(condition);
 const printQuestion = (question) => console.log(`Question: ${question}`);
 const printWrongAnswer = (correct, wrong) => (
-    console.log(`'${wrong}' is wrong answer ;(. Correct answer was '${correct}'.`)
-)
+  console.log(`'${wrong}' is wrong answer ;(. Correct answer was '${correct}'.`)
+);
 
 const printGameOver = (name, correct, wrong) => {
-    printWrongAnswer(correct, wrong);
-    printLoseGame(name)
-}
+  printWrongAnswer(correct, wrong);
+  printLoseGame(name);
+};
 
-const getAnswer = () => readlineSync.question(`Your answer: `).trim().toLowerCase();
+const getAnswer = () => readlineSync.question('Your answer: ').trim().toLowerCase();
 
 export const playGame = (conditions, questionGenerator) => () => {
-    const playerName = greetPlayer();
+  const playerName = greetPlayer();
 
-    printConditions(conditions);
+  printConditions(conditions);
 
-    const playRound = (round, correctAnswer = false) => {
-        if (!round) { return printWinGame(playerName) }
-        if (correctAnswer) { printCorrect() }
-        const [question, correctAns] = questionGenerator();
-        printQuestion(question);
-        const userAns = getAnswer();
-        return (
-            userAns === correctAns ?
-                playRound(--round, true) :
-                printGameOver(playerName, correctAns, userAns)
-        );
-    }
+  const playRound = (round, correctAnswer = false) => {
+    if (!round) { return printWinGame(playerName); }
+    if (correctAnswer) { printCorrect(); }
+    const [question, correctAns] = questionGenerator();
+    printQuestion(question);
+    const userAns = getAnswer();
+    return (
+      userAns === correctAns
+        ? playRound(dec(round), true)
+        : printGameOver(playerName, correctAns, userAns)
+    );
+  };
 
-    return playRound(GAME_ROUND)
-}
+  return playRound(GAME_ROUND);
+};
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,11 @@
-export const randomInt = (max= 1000, min = 0) => (
-    Math.floor(min + (Math.random() * (max - min)))
+export const randomInt = (max = 1000, min = 0) => (
+  Math.floor(min + (Math.random() * (max - min)))
 );
 
 export const randomElement = (arr) => arr[randomInt(arr.length - 1)];
 
 export const last = (arr) => arr[arr.length - 1];
+
+export const dec = (n, step = 1) => n - step;
+
+export const inc = (n, step = 1) => n + step;
