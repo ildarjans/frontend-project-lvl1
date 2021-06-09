fa65cb6638c01699b9b985761bebfb05ebb2befd
ildarjans <nobap84@rambler.ru>
2021-06-09 14:52:17 +0300

    fix edge case

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -4,12 +4,13 @@ import { randomInt } from '../utils.js';
 const TASK_CONDITION = 'Find the greatest common divisor of given numbers.';
 
 const MAX_GCD_NUM = 100;
+const MIN_GCD_NUM = 1;
 
 const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b));
 
 const getQuestionAnswerPair = () => {
-  const a = randomInt(MAX_GCD_NUM);
-  const b = randomInt(MAX_GCD_NUM);
+  const a = randomInt(MAX_GCD_NUM, MIN_GCD_NUM);
+  const b = randomInt(MAX_GCD_NUM, MIN_GCD_NUM);
   return [`${a} ${b}`, `${gcd(a, b)}`];
 };
 
